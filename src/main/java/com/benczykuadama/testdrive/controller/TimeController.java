227b7bb2c0f17befd2db04c6da49780cf30c2130
patiem <pp.mikulska@gmail.com>
2018-04-04 18:17:38 +0200

    Add one test for TimeController for path without parameter

--- src/main/java/com/benczykuadama/testdrive/controller/TimeController.java
+++ src/main/java/com/benczykuadama/testdrive/controller/TimeController.java
@@ -1,7 +1,6 @@
 package com.benczykuadama.testdrive.controller;
 
 import com.benczykuadama.testdrive.model.Time;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/test/java/com/benczykuadama/testdrive/controller/TimeControllerTest.java
@@ -0,0 +1,34 @@
+package com.benczykuadama.testdrive.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TimeControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void returnsJsonWithDefaultName_whenNameValueNotProvided() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.get("/time").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", equalTo(1)))
+                .andExpect(jsonPath("$.message", equalTo("You are still alive, my dear filthy worm!")));
+    }
+
+}
