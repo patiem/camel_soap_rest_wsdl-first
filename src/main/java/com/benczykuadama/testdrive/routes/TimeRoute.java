57a126238fca607f9c10e4ed5bd569993b6b6766
patiem <pp.mikulska@gmail.com>
2018-04-11 13:12:39 +0200

    Remove test and add empty camel rout class TimeRoute

---
+++ src/main/java/com/benczykuadama/testdrive/routes/TimeRoute.java
@@ -0,0 +1,11 @@
+package com.benczykuadama.testdrive.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class TimeRoute extends RouteBuilder {
+    
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
