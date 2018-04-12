d7c4d0d03fa9237fc92ae20dce0ae85f09187bc6
patiem <pp.mikulska@gmail.com>
2018-04-12 16:17:25 +0200

    Get multiple parameters from url

--- src/main/java/com/benczykuadama/testdrive/routes/TimeRoute.java
+++ src/main/java/com/benczykuadama/testdrive/routes/TimeRoute.java
@@ -2,24 +2,19 @@ package com.benczykuadama.testdrive.routes;
 
 import com.benczykuadama.testdrive.model.Time;
 import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.apache.camel.model.rest.RestParamType;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.cxf.jaxrs.utils.JAXRSUtils;
 import org.springframework.stereotype.Component;
 
+import javax.ws.rs.core.MultivaluedMap;
 import java.time.LocalDateTime;
+
 
 @Component
 public class TimeRoute extends RouteBuilder {
-
-//    @Value("${port}")
-//    private int port;
-//
-//    @Value("${host}")
-//    private String host;
 
     @Override
     public void configure() throws Exception {
@@ -35,16 +30,28 @@ public class TimeRoute extends RouteBuilder {
 
 
         from("rest:get:/processtime")
-                .process(exchange ->  {
-                        Time time = new Time();
-                        String name = exchange.getIn().getHeader("name", "My PRECIOUS", String.class);
-                        time.setMessage(name);
-                        exchange.getOut().setBody(time);
+                .process(exchange -> {
+                    Time time = new Time();
+                    String name = exchange.getIn().getHeader("name", "My PRECIOUS", String.class);
+                    time.setMessage(name);
+                    exchange.getOut().setBody(time);
                 })
-                .process( exchange -> {
-                        Time time = exchange.getIn().getBody(Time.class);
-                        time.setNow(LocalDateTime.now());
-                        exchange.getOut().setBody(time);
+                .process(exchange -> {
+                    Time time = exchange.getIn().getBody(Time.class);
+                    time.setNow(LocalDateTime.now());
+                    exchange.getOut().setBody(time);
+                })
+                .marshal().json(JsonLibrary.Jackson);
+
+
+        from("rest:get:manytime")
+                .process(exchange -> {
+                    String queryString = exchange.getIn().getHeader(Exchange.HTTP_QUERY, String.class);
+                    MultivaluedMap<String, String> queryMap = JAXRSUtils.getStructuredParams(queryString, "&", false, false);
+                    System.out.println(queryString);
+                    System.out.println(queryMap);
+                    for(String param : queryMap.keySet()) System.out.println(queryMap.getFirst(param));
+
                 })
                 .marshal().json(JsonLibrary.Jackson);
     }
