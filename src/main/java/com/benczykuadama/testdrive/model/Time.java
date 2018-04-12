2d18e8d074c1e04fca0581a008272b047cf622c9
patiem <pp.mikulska@gmail.com>
2018-04-12 14:00:41 +0200

    Add rest path cameltime and two processes for processtime

--- src/main/java/com/benczykuadama/testdrive/model/Time.java
+++ src/main/java/com/benczykuadama/testdrive/model/Time.java
@@ -2,20 +2,25 @@ package com.benczykuadama.testdrive.model;
 
 
 import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class Time {
+
+    private static final String template = "You are still alive, my dear %s!";
+    private static final AtomicInteger counter = new AtomicInteger();
 
     private Integer id;
     private LocalDateTime now;
     private String message;
 
     public Time() {
+        id = counter.incrementAndGet();
     }
 
     public Time(Integer id, String message, LocalDateTime now) {
         this.id = id;
-        this.message = message;
+        this.message = String.format(template, message);
         this.now = now;
     }
 
@@ -40,7 +45,7 @@ public class Time {
     }
 
     public void setMessage(String message) {
-        this.message = message;
+        this.message = String.format(template, message);
     }
 
 }
--- src/main/java/com/benczykuadama/testdrive/routes/TimeRoute.java
+++ src/main/java/com/benczykuadama/testdrive/routes/TimeRoute.java
@@ -1,10 +1,16 @@
 package com.benczykuadama.testdrive.routes;
 
+import com.benczykuadama.testdrive.model.Time;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.apache.camel.model.rest.RestParamType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class TimeRoute extends RouteBuilder {
@@ -27,5 +33,19 @@ public class TimeRoute extends RouteBuilder {
                 .param().name("name").defaultValue("filthy worm").type(RestParamType.query).dataType("String").endParam()
                 .to("bean:timeService?method=getTime(${header.name})");
 
+
+        from("rest:get:/processtime")
+                .process(exchange ->  {
+                        Time time = new Time();
+                        String name = exchange.getIn().getHeader("name", "My PRECIOUS", String.class);
+                        time.setMessage(name);
+                        exchange.getOut().setBody(time);
+                })
+                .process( exchange -> {
+                        Time time = exchange.getIn().getBody(Time.class);
+                        time.setNow(LocalDateTime.now());
+                        exchange.getOut().setBody(time);
+                })
+                .marshal().json(JsonLibrary.Jackson);
     }
 }
